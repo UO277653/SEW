960dbe3289754327c2a9e31d03cc92831a9cf609
UO277653 <79276201+UO277653@users.noreply.github.com>
2021-11-30 00:22:29 +0100

    Add files via upload

--- PRACTICA-3/Ejercicio-02/MasInfoNavegador.js
+++ PRACTICA-3/Ejercicio-02/MasInfoNavegador.js
@@ -1,14 +1,14 @@
 document.write("Versión: ");
 document.write(infoNavegador.version);
-document.write("<br>");
+document.write(" ");
 document.write("Plataforma: ");
 document.write(infoNavegador.plataforma);
-document.write("<br>");
+document.write(" ");
 document.write("Vendedor: ");
 document.write(infoNavegador.vendedor);
-document.write("<br>");
+document.write(" ");
 document.write("Agente: ");
 document.write(infoNavegador.agente);
-document.write("<br>");
+document.write(" ");
 document.write("Java activo: ");
 document.write(infoNavegador.javaActivo);
