86a133c414baa5f7f71ddf082e13b04acd1d3113
UO277653 <79276201+UO277653@users.noreply.github.com>
2021-11-29 23:15:25 +0100

    Add files via upload

--- PRACTICA-3/Ejercicio-01/Parrafos.js
+++ PRACTICA-3/Ejercicio-01/Parrafos.js
@@ -1,5 +1,9 @@
-document.write("<p>");
+document.write("<p>Curso: ");
 document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p>Alumno: ");
 document.write(asignatura.nombre);
+document.write("</p>");
+document.write("<p>Email: ");
 document.write(asignatura.email);
 document.write("</p>");
